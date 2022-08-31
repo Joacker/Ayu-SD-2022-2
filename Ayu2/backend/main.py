32e01315000b506bbfdc8e7a7986c5b44a20a361
Joaquín Fernández <67400456+Joacker@users.noreply.github.com>
2022-08-30 22:01:14 -0400

    new commit

--- Ayu2/backend/main.py
+++ Ayu2/backend/main.py
@@ -1,1 +1,21 @@
-print("el viernes la hago, ahora me da paja y tengo cosas que hacer :)")
+import redis, time
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_his_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+def hello():
+    count = get_his_count()
+    return ('Hello World! I have been seen {} times.\n'.format(count))
