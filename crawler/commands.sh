dd2934ae745e26968acd32cc5b03e30edb7b522b
Joaquín Fernández <67400456+Joacker@users.noreply.github.com>
2022-09-14 23:13:20 -0300

    commands

---
+++ crawler/commands.sh
@@ -0,0 +1,8 @@
+mkdir logs
+cd logs
+wget http://www.cim.mcgill.ca/~dudek/206/Logs/AOL-user-ct-collection/user-ct-test-collection-09.txt.gz
+gunzip user-ct-test-collection-09.txt.gz
+rm -f user-ct-test-collection-09.txt.gz
+mv user-ct-test-collection-09.txt file.txt
+cd ..
+python3 myversion.py
