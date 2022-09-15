ea5e565b2e254ebaa135e43b61192aa508dfed5a
Joaquín Fernández <67400456+Joacker@users.noreply.github.com>
2022-09-15 00:22:15 -0300

    new commit

--- crawler/commands.sh
+++ crawler/commands.sh
@@ -1,3 +1,4 @@
+pip3 install BeautifulSoup4
 mkdir logs
 cd logs
 wget http://www.cim.mcgill.ca/~dudek/206/Logs/AOL-user-ct-collection/user-ct-test-collection-09.txt.gz
@@ -6,3 +7,4 @@ rm -f user-ct-test-collection-09.txt.gz
 mv user-ct-test-collection-09.txt file.txt
 cd ..
 python3 myversion.py
+rm -rf logs
